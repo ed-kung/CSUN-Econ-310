7da182732a465509ec85712e104633a678b7f497
Edward Kung <edward.kung@gusto.com>
2024-06-20 13:23:52 -0700

    Update tools for auto generating problems

--- _workspace/utils.py
+++ _workspace/utils.py
@@ -1,7 +1,33 @@
 import numpy as np
 
+def polyeq(var='x', coefs=[1,1,1], remove_first_plus_minus=True):
+    t = ''
+    for p in range(len(coefs)):
+        c = coefs[p]
+        if c!=0:
+            if c>0:
+                t+='+ '
+            else:
+                t+='- '
+            if p==0:
+                t+=f'{np.abs(c)} '
+            else:
+                if np.abs(c)!=1:
+                    t+=f'{np.abs(c)}'
+                if p==1:
+                    t+=f'{var} '
+                else:
+                    t+=f'{var}^{p}'
+    if remove_first_plus_minus:
+        return t[2:]
+    else:
+        return t
+
 class SREQ:
     def __init__(self,N=3000,M=200,Y=100,alpha=10,beta=2,gamma=0,delta=0,eta=0.2):
+        Q = (alpha - delta)/(beta/N + eta/M)
+        p = (N*eta*alpha + M*beta*delta)/(N*eta + M*beta)
+        
         self.N=N
         self.M=M
         self.Y=Y
@@ -10,30 +36,212 @@ class SREQ:
         self.gamma=gamma
         self.delta=delta
         self.eta=eta
-    
-    def solve(self):
-        self.Q = (self.alpha - self.delta) / (self.beta/self.N + self.eta/self.M)
-        self.p = (self.N*self.eta*self.alpha + self.M*self.beta*self.delta)/(self.N*self.eta + self.M*self.beta)
-        self.qd = self.Q / self.N
-        self.qs = self.Q / self.M
-        self.c = self.Y - self.p*self.qd
-        self.revenue = self.p * self.qs
-        self.cost = self.gamma + self.delta*self.qs + 0.5*self.eta*self.qs**2
+        self.Q = Q
+        self.p = p
+        self.qd = Q/N
+        self.qs = Q/M
+        self.c = Y - p*self.qd
+        self.revenue = p*self.qs 
+        self.cost = gamma + delta*self.qs + 0.5*self.eta*self.qs**2
         self.profit = self.revenue - self.cost
-        self.totalprofit = self.M * self.profit
-        self.util = self.c + self.alpha*self.qd - 0.5*self.beta*self.qd**2
-        self.totalutil = self.N*self.util
-        return {
-            "Q": self.Q, 
-            "p": self.p,
-            "qd": self.qd,
-            "qs": self.qs,
-            "c": self.c,
-            "profit": self.profit,
-            "totalprofit": self.totalprofit,
-            "util": self.util,
-            "totalutil": self.totalutil
-        }
+        self.totalprofit = M*self.profit
+        self.util = self.c + alpha*self.qd - 0.5*beta*self.qd**2
+        self.totalutil = N*self.util
+        self.total_surplus = self.totalprofit + self.totalutil
+        
+    def check_solution(self):
+        return (
+            (self.c>0) and
+            (self.qd>0) and
+            (self.qs>0) and
+            (self.p>0) and
+            (np.abs(self.p%1)<0.001) and
+            (np.abs(self.qd%1)<0.001) and
+            (np.abs(self.qs%1)<0.001)
+        )
+
+    def general_setup(self):
+        return r"""
+A commodity $q$ is traded at price $p$ in a competitive market with $N$ identical consumers and $M$ identical firms.\\
+
+Each consumer has income $Y$ and maximizes:
+
+\begin{align*}
+\max_{c,q} ~ c + \alpha q - \tfrac{1}{2} \beta q^2 ~ \text{ s.t. } c + pq = Y
+\end{align*}
+
+Each firm's cost function is:
+
+\begin{align*}
+c(q) = \gamma + \delta q + \tfrac{1}{2} \eta q^2
+\end{align*}
+
+The firm maximizes:
+
+\begin{align*}
+\max_{q} ~ pq - c(q)
+\end{align*}
+
+The short run equilibrium condition is:
+
+\begin{align*}
+Nq_d = Mq_s = Q
+\end{align*}
+
+The general solutions are:
+
+\begin{align*}
+Q = \frac{\alpha - \delta}{\beta/N + \eta/M} 
+\end{align*}
+
+\begin{align*}
+p = \frac{N \eta \alpha + M \beta \delta}{N \eta + M \beta}
+\end{align*}
+"""
+
+    def setup(self):
+        return r"""
+A commodity $q$ is traded at price $p$ in a competitive market with price-taking consumers and firms. \\
+        
+There are ${:,.0f}$ identical consumers each with income $Y={:,.0f}$. Each consumer has a utility function over numeraire consumption $c$ and commodity $q$ given by:
+
+$$u(c,q) = c + {}$$
+
+There are ${:,.0f}$ identical firms each with cost function given by:
+
+$$c(q) = {}$$
+""".format(
+    self.N, 
+    self.Y,
+    polyeq('q', [0, self.alpha, -0.5*self.beta]),
+    self.M,
+    polyeq('q', [self.gamma, self.delta, 0.5*self.eta])
+)
+
+    def solution(self):
+        return r"""
+The solutions are: $p={}$, $q_d={}$, $q_s={}$.
+""".format(self.p, self.qd, self.qs)
+
+        
+class AdValoremSR:
+    def __init__(self,N=3000,M=200,Y=100,alpha=10,beta=2,gamma=0,delta=0,eta=0.2,tc=0.1,tp=0):
+        Q = ((1-tp)*alpha - (1+tc)*delta)/((1+tc)*eta/M + (1-tp)*beta/N)
+        p = (N*eta*alpha + M*beta*delta)/(N*eta*(1+tc) + M*beta*(1-tp))
+        
+        self.N=N
+        self.M=M
+        self.Y=Y
+        self.alpha=alpha
+        self.beta=beta
+        self.gamma=gamma
+        self.delta=delta
+        self.eta=eta
+        self.tc=tc
+        self.tp=tp
+        self.Q = Q
+        self.p = p
+        self.qd = Q/N
+        self.qs = Q/M
+        self.c = Y - p*self.qd
+        self.revenue = p*self.qs 
+        self.cost = gamma + delta*self.qs + 0.5*self.eta*self.qs**2
+        self.profit = self.revenue - self.cost
+        self.totalprofit = M*self.profit
+        self.util = self.c + alpha*self.qd - 0.5*beta*self.qd**2
+        self.totalutil = N*self.util
+        self.tax_revenue = (tc+tp)*Q
+        self.total_surplus = self.totalutil + self.totalprofit + self.tax_revenue
+        self.eq_notax = SREQ(N=N,M=M,Y=Y,alpha=alpha,beta=beta,gamma=gamma,delta=delta,eta=eta)
+        self.DWL = self.eq_notax.total_surplus - self.total_surplus
+
+    def check_solution(self):
+        return (
+            (self.c>0) and
+            (self.p>0) and
+            (self.qd>0) and
+            (self.qs>0) and
+            (np.abs(self.p%1)<0.0001) and
+            (np.abs(self.qd%1)<0.0001) and
+            (np.abs(self.qs%1)<0.0001) and
+            (self.eq_notax.check_solution())
+        )
+
+    def general_setup(self):
+        return r"""
+A commodity $q$ is traded at price $p$ in a competitive market with $N$ identical consumers and $M$ identical firms.\\
+
+An ad-valorem tax rate of $t_c$ is placed on consumers, and an ad-valorem tax rate of $t_p$ is placed on producers.
+
+Each consumer has income $Y$ and maximizes:
+
+\begin{align*}
+\max_{c,q} ~ c + \alpha q - \tfrac{1}{2} \beta q^2 ~ \text{ s.t. } c + (1+t_c)pq = Y
+\end{align*}
+
+Each firm's cost function is:
+
+\begin{align*}
+c(q) = \gamma + \delta q + \tfrac{1}{2} \eta q^2
+\end{align*}
+
+The firm maximizes:
+
+\begin{align*}
+\max_{q} ~ (1-t_p)pq - c(q)
+\end{align*}
+
+The short run equilibrium condition is:
+
+\begin{align*}
+Nq_d = Mq_s = Q
+\end{align*}
+
+The general solutions are:
+
+\begin{align*}
+Q = \frac{(1-t_p)\alpha - (1+t_c)\delta}{(1-t_p)\beta/N + (1+t_c)\eta/M} 
+\end{align*}
+
+\begin{align*}
+p = \frac{N \eta \alpha + M \beta \delta}{(1+t_c) N \eta + (1-t_p) M \beta}
+\end{align*}
+"""
+
+    def setup(self):
+        t = r"""
+A commodity $q$ is traded at price $p$ in a competitive market with price-taking consumers and firms. \\
+        
+There are ${:,.0f}$ identical consumers each with income $Y={:,.0f}$. Each consumer has a utility function over numeraire consumption $c$ and commodity $q$ given by:
+
+$$u(c,q) = c + {}$$
+
+There are ${:,.0f}$ identical firms each with cost function given by:
+
+$$c(q) = {}$$
+
+""".format(
+    self.N, 
+    self.Y,
+    polyeq('q', [0, self.alpha, -0.5*self.beta]),
+    self.M,
+    polyeq('q', [self.gamma, self.delta, 0.5*self.eta])
+)
+        if self.tc>0:
+            t+= r"An ad-valorem tax rate of ${:.0f}\%$ is placed on consumers.\\".format(self.tc*100)
+        if self.tp>0:
+            t+= r"An ad-valorem tax rate of ${:.0f}\%$ is placed on producers.\\".format(self.tp*100)
+        return t
+
+    def solution(self):
+        return r"""
+Solutions: \\
+
+Pre-tax: $p={}$, $q_d={}$, $q_s={}$ \\
+
+Post-tax: $p={}$, $q_d={}$, $q_s={}$
+""".format(self.eq_notax.p, self.eq_notax.qd, self.eq_notax.qs, self.p, self.qd, self.qs)
+
 
 class LREQ:
     def __init__(self,N=3000,Y=100,alpha=10,beta=2,gamma=32,delta=0,eta=0.2):
@@ -114,5 +322,46 @@ class CobbDouglasConsumer:
             (self.xmax%1==0) and
             (self.ymax%1==0)
         )
-    
+
+class CobbDouglasDeflator:
+    def __init__(self,a,b,I,px0,py0,px1,py1):
+        CPI_DEFLATOR = (1/(1+b/a))*(px1/px0) + (1/(1+a/b))*(py1/py0)
+        GOOD_DEFLATOR = (px1/px0)**(a/(a+b)) * (py1/py0)**(b/(a+b))
         
+        cb0 = CobbDouglasConsumer(a=a,b=b,I=I,px=px0,py=py0)
+        cb1 = CobbDouglasConsumer(a=a,b=b,I=I*GOOD_DEFLATOR,px=px1,py=py1)
+
+        assert np.abs(CPI_DEFLATOR - (px1*cb0.x+py1*cb0.y)/(px0*cb0.x+py0*cb0.y))<0.0001
+        assert np.abs(cb0.U - cb1.U)<0.0001
+
+        self.a = a
+        self.b = b
+        self.I = I
+        self.px0 = px0
+        self.py0 = py0
+        self.px1 = px1
+        self.py1 = py1
+        self.CPI_DEFLATOR = CPI_DEFLATOR
+        self.GOOD_DEFLATOR = GOOD_DEFLATOR
+        self.cb0 = cb0
+        self.cb1 = cb1
+
+    def __repr__(self):
+        return repr({
+            'a': self.a,
+            'b': self.b,
+            'I': self.I,
+            'px0': self.px0,
+            'py0': self.py0,
+            'px1': self.px1,
+            'py1': self.py1,
+            'CPI_DEFLATOR': self.CPI_DEFLATOR,
+            'GOOD_DEFLATOR': self.GOOD_DEFLATOR
+        })
+
+    def is_integer(self):
+        return (
+            (self.cb0.x%1==0) and
+            (self.cb0.y%1==0) 
+        )
+
--- docs/notes/14-price-deflators.md
+++ docs/notes/14-price-deflators.md
@@ -1,34 +1,99 @@
 ---
 layout: default
-title: "14. Application: Price Deflators and Real Income"
+title: "14. Application: Issues with the CPI"
 parent: Lecture Notes
 nav_order: 14
 ---
 
-# Application: Price Deflators and Real Income
+# Application: Issues with the CPI
 {: .no_toc }
 
 - TOC
 {:toc}
 
+## What is the CPI?
+
+CPI stands for the **Consumer Price Index**.  The CPI is a measure of *aggregate* price levels. That is, when the prices of many goods are changing all at the same time, and in different ways, the CPI gives us a sense of how much these prices changed *on average*
+
+CPI is measured between two periods: a base period and a comparison period. It is calculated by first selecting a fixed basket of goods, then calculating the total cost to buy that basket in the comparison period compared to the base period:
+
+$$ \text{CPI} = \frac{\text{Cost of basket in comparison period}}{\text{Cost of basket in base period}} $$
+
+{: .blue-callout-title }
+> CPI Example
+>
+> There are two goods, $$x$$ and $$y$$.  In 2020 (the base period), the prices of the goods were $$p_x = 10$$ and $$p_y=5$$. In 2024 (the comparison period), the prices of the goods were $$p_x=12$$ and $$p_y=7$$. Calculate the CPI between 2020 and 2024 using a basket with $$x=5$$ and $$y=5$$.
+>
+> *Answer.*
+>
+> $$\begin{align}
+\text{CPI} &= \frac{\text{Cost of basket in comparison period}}{\text{Cost of basket in base period}} \\
+&= \frac{12 \cdot 5 + 7 \cdot 5}{10 \cdot 5 + 5 \cdot 5} \\
+&= 1.267
+\end{align}$$
+>
+> In other words, the cost of the basket increased by 26.7%.
+
+
+A **price deflator** is a measure of *aggregate* price levels. That is, when the prices of many goods are changing all at the same time, and in different ways, a price deflator gives us a sense of how much these prices changed *on average*.  
+
+A price deflator is always measured between a *base period* and a *comparison period*. And roughly speaking, it is always measured as the change in income required to maintain a constant quality of life (QoL) between the two periods.
+
+$$ \text{Price Deflator} = \frac{\text{Income required in comparison period to maintain QoL}}{\text{Income in base period}} $$
+
+In this lecture, we will consider two different ways to calculate price deflators. The two ways differ in their concept of maintaining constant QoL. In the first way, constant QoL is defined as maintaining the exact same consumption bundle. In the second way, constant QoL is defined as maintaining the same utility. We'll discuss the strengths and weaknesses of each approach.
 
 
 
-## Income and substitution effects
-
-Consumers don't make purchasing decisions in isolation. The price of gasoline doesn't just affect a consumer's demand for gasoline, it also affects their overall pocketbook and thus affects how much they spend on groceries, entertainment, clothing, and travel.
+## Constant Bundle Deflator
 
 
+## Constant Utility Deflator
 
 
 {: .purple-callout-title }
 > Economic Insight
 >
-> When the price of good $$x$$ increases/decreases, there are two effects:
+> The constant-utility deflator and the constant-bundle deflator are different. This is because consumers don't have to maintain the exact same consumption bundle in order to maintain the same utility.  When prices change, consumers can substitute between goods in a way that maintains their utility even if some goods become more or less expensive.
+
+{: .blue-callout-title }
+> Example X
 >
-> 1. *The income effect*. Since the price of $$x$$ has increased/decreased, the consumer is overall poorer/richer. This causes him to buy less/more of *all* goods.
+> This example illustrates the above insight. Consider a consumer with utility function:
 >
-> 2. *The substitution effect*. Since the price of $$x$$ has increased/decreased, good $$x$$ becomes less/more attractive relative to other goods. This causes him to buy less/more of good $$x$$ and more/less of other goods.
+> $$u(x,y) = x + y$$
+> 
+> $$x$$ and $$y$$ are perfect substitutes and the indifference curves are plotted in the diagram below.
+>
+> *Diagram*
+>
+> Suppose that in the base period, income is $$I=10$$ and $$p_x=1$$ and $$p_y=2$$. Then, in the comparison period, income stays the same but prices become $$p_x=2$$ and  $$p_y=1$$.
+>
+> Show that the constant-bundle deflator is equal to $$2$$ but the constant-utility deflator is equal to $$1$$.
+>
+> *Answer.*
+>
+> First, let's find the optimal choice of $$x$$ and $$y$$ in the base period by drawing the budget line on the diagram.
+>
+> *Diagram*
+>
+> The optimal choice is $$x=10$$, $$y=0$$. Utility is $$u(x,y)=10$$. The constant bundle deflator is:
+>
+> $$\begin{align}
+\text{Constant Bundle Deflator} &= \frac{p_x^\prime x + p_y^\prime y}{p_x x + p_y y} \\
+&= \frac{2(10) + 1(0)}{1(10) + 2(0)} \\
+&= 2
+\end{align}$$
+>
+> But let's consider what the consumer would actually choose in the comparison period. The diagram below shows the budget line in the comparison period. 
+>
+> *Diagram*
+>
+> In the comparison period, the optimal choice is $$x=0$$, $$y=10$$. The utility is $$u(x,y)=10$$. So the income required to achieve a constant utility 
+>
+> This example therefore shows that when there is a high degree of substitutability between goods, the constant-bundle deflator may not be a good QoL adjustment.
+
+
 
 
 
